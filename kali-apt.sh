06e5f5070dca2fb73cdf4d1f517ab89c730328dd
internet-goods <4577617+internet-goods@users.noreply.github.com>
2022-10-08 14:10:41 -0700

    eaphammer

--- kali-apt.sh
+++ kali-apt.sh
@@ -2,5 +2,8 @@
 apt-get -y install checksec
 apt-get -y install hcxtools
 apt-get -y install ghidra 
+apt-get -y install airgeddon
+apt-get -y install btscanner
+apt-get -y install eaphammer
 
 apt -y autoremove 
