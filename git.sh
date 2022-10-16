6fce517799f31ed19433682871184c18122a779f
internet-goods <4577617+internet-goods@users.noreply.github.com>
2022-10-15 18:36:16 -0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -6,3 +6,7 @@ git clone https://github.com/neural75/gqrx-scanner
 git clone https://github.com/dxa4481/WPA2-HalfHandshake-Crack
 git clone https://github.com/brannondorsey/wifi-cracking
 git clone https://github.com/routerkeygen/routerkeygenPC
+git clone https://github.com/JPaulMora/Pyrit
+git clone https://github.com/BastilleResearch/gr-lora
+git clone https://github.com/travisgoodspeed/goodfet
+git clone https://github.com/soxrok2212/PSKracker
