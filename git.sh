76f08a5b55e450f69c8e5e2aa957f283780def47
internet-goods <4577617+internet-goods@users.noreply.github.com>
2022-10-15 18:12:57 -0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -3,3 +3,4 @@ cd git
 git clone https://github.com/praetorian-inc/Hob0Rules
 git clone https://github.com/brannondorsey/naive-hashcat
 git clone https://github.com/neural75/gqrx-scanner
+git clone https://github.com/dxa4481/WPA2-HalfHandshake-Crack
