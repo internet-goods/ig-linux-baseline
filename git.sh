904b7a41bc10c874d1eb67da3b74e4a905f6ad5c
internet-goods <4577617+internet-goods@users.noreply.github.com>
2022-10-15 18:45:29 -0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -10,3 +10,5 @@ git clone https://github.com/JPaulMora/Pyrit
 git clone https://github.com/BastilleResearch/gr-lora
 git clone https://github.com/travisgoodspeed/goodfet
 git clone https://github.com/soxrok2212/PSKracker
+git clone https://github.com/MS-WEB-BN/h4rpy
+git clone https://github.com/philcryer/wpa2own
