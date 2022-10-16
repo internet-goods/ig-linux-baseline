0bb9b560ff8f0e7795945c24ae6feccb75ced03d
internet-goods <4577617+internet-goods@users.noreply.github.com>
2022-10-15 17:52:21 -0700

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,5 @@
+mkdir git
+cd git
+git clone https://github.com/praetorian-inc/Hob0Rules
+git clone https://github.com/brannondorsey/naive-hashcat
+git clone https://github.com/neural75/gqrx-scanner
