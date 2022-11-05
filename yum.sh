607f29a1b4b21d1c961e54815c2b636890ff412e
internet-goods <4577617+internet-goods@users.noreply.github.com>
2022-11-04 18:55:31 -0700

    Create yum.sh

---
+++ yum.sh
@@ -0,0 +1,7 @@
+yum -y install iotop htop sysstat ccze lsof
+yum -y install screen
+yum -y install lynis rng-tools mlocate 
+yum -y install lm_sensors smartmontools
+yum -y install alpine
+yum -y install masscan nmap
+yum -y install iw wavemon
