edc7f1ab731bcc7db1a96e0a4736547c30275068
internet-goods <4577617+internet-goods@users.noreply.github.com>
2022-11-04 20:05:50 -0700

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -7,4 +7,5 @@ yum -y install masscan nmap
 yum -y install iw wavemon
 yum -y install gcc
 #aircrack
-yum install sqlite-devel openssl-devel libpcap-devel pcre-devel rfkill libnl3-devel ethtool hwloc-devel libcmocka-devel make file expect hostapd wpa_supplicant iw usbutils tcpdump screen zlib-devel
+yum -y install sqlite-devel openssl-devel libpcap-devel pcre-devel rfkill libnl3-devel ethtool hwloc-devel libcmocka-devel make file expect hostapd wpa_supplicant iw usbutils tcpdump screen zlib-devel
+yum -y install libtool
