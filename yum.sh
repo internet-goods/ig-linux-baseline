715fbc87dca167285c2ad5b7de2d8dee4389f678
internet-goods <4577617+internet-goods@users.noreply.github.com>
2022-11-06 00:41:58 -0500

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -9,3 +9,4 @@ yum -y install gcc
 #aircrack
 yum -y install sqlite-devel openssl-devel libpcap-devel pcre-devel rfkill libnl3-devel ethtool hwloc-devel libcmocka-devel make file expect hostapd wpa_supplicant iw usbutils tcpdump screen zlib-devel
 yum -y install ncurses-devel libnl-devel
+yum -y install sysbench
