b493e60523d09ae8a1ec6a65a4cc817ced3ddc6e
internet-goods <4577617+internet-goods@users.noreply.github.com>
2023-02-09 19:32:45 -0600

    Update debian-apt.sh

--- debian-apt.sh
+++ debian-apt.sh
@@ -67,3 +67,12 @@ apt-get -y install lynis
 apt -y install qemu-kvm qemu-system-arm qemu-system-x86 virt-manager
 apt -y install speedtest-cli
 apt -y install mdadm
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+# 2. Add our repository to your list of repositories
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+# 3. Update your package database and install signal
+sudo apt update && sudo apt install signal-desktop
