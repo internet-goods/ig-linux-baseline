5ef2ae792e341c3c493b4db8dfd5b7577eedb9ab
internet-goods <4577617+internet-goods@users.noreply.github.com>
2023-02-12 13:04:20 -0600

    Update git.sh

--- git.sh
+++ git.sh
@@ -37,3 +37,4 @@ git clone https://github.com/sherlly/PCRT
 git clone https://github.com/DEMON1A/CTF-RSA-Solver
 git clone https://github.com/payloadbox/sql-injection-payload-list
 git clone https://github.com/danielcardeenas/AudioStego
+git clone https://github.com/chrislockard/api_wordlist
