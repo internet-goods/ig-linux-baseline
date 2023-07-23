e46ad98c0f96c78407c6187f3a361a51d795fcd1
internet-goods <4577617+internet-goods@users.noreply.github.com>
2023-07-23 17:13:22 -0500

    Update pentoo-emerge.sh

--- pentoo-emerge.sh
+++ pentoo-emerge.sh
@@ -1,33 +1,41 @@
+#daemons to configure
 emerge mdadm
-emerge dclfdd
-emerge terminator
+emerge mail-mta/sendmail
+emerge net-analyzer/suricata
+emerge app-antivirus/clamav
+emerge sys-process/audit
+emerge net-proxy/privoxy
 emerge logrotate
+#apps to run
+#dd has status=progress now emerge dclfdd
+emerge terminator
 emerge media-gfx/gimp
 emerge media-gfx/geeqie
 emerge app-crypt/steghide
 emerge app-misc/binwalk
+emerge net-analyzer/arp-scan
 emerge net-analyzer/dirsearch
 emerge net-analyzer/ffuf
 emerge net-analyzer/masscan
 emerge net-analyzer/testssl
 emerge net-analyzer/xsstrike
 emerge net-analyzer/iftop
-emerge net-analyzer/suricata
+emerge net-analyzer/multimon-ng
 emerge net-wireless/uhd
 emerge net-wireless/airgeddon
 emerge net-wireless/wavemon
 emerge net-wireless/eaphammer
+emerge net-wireless/urh
 emerge sys-apps/hwinfo
 emerge net-im/signal-desktop-bin
 emerge media-video/mpv
 emerge media-tv/v4l-utils
+emerge media-sound/audacity
 emerge libheif
 emerge app-arch/bzip2
 emerge lshw
 emerge app-misc/mc
-#segfaults emerge net-wireless/btscanner
 emerge net-mail/swaks
-net-analyzer/arp-scan
 emerge =dev-lang/python-2*
 emerge dev-python/pip
 emerge dev-python/notebook
@@ -38,3 +46,7 @@ emerge net-irc/irssi
 emerge dev-python/csvkit
 emerge app-admin/keepassxc
 emerge www-client/google-chrome-unstable
+emerge media-sound/supercollider
+#unstable packages segfault or wont install
+#emerge net-wireless/btscanner
+#emerge net-wireless/horst
