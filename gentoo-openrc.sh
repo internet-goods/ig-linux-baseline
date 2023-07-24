be9a2824f7bdde1fdc8175068b2dd6ed9f7ae0ec
internet-goods <4577617+internet-goods@users.noreply.github.com>
2023-07-24 11:45:57 -0500

    Update gentoo-openrc.sh

--- gentoo-openrc.sh
+++ gentoo-openrc.sh
@@ -1,3 +1,9 @@
+rc-service sshd start
+rc-update add sshd
+rc-service iptables start
+rc-update add iptables 
+rc-service ip6tables start
+rc-update add ip6tables
 rc-service smartd start
 rc-update add smartd
 rc-service smartd sendmail
@@ -13,4 +19,7 @@ rc-service lm_sensors start
 rc-update add sensord
 rc-service sensord start
 rc-update add mdraid boot
+rc-service privoxy start
+rc-update add privoxy
 rc-update -v show
+rc-status
