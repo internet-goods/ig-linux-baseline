aaf7bb8670671988725ce35e529fa962ebeab272
internet-goods <4577617+internet-goods@users.noreply.github.com>
2023-10-21 20:49:52 -0500

    Update pentoo-baseline.sh

--- pentoo-baseline.sh
+++ pentoo-baseline.sh
@@ -2,10 +2,7 @@
 bash ./pentoo-emerge.sh
 bash ./gentoo-openrc.sh
 bash ./gentoo-localtime.sh
-
-mkdir /etc/NetworkManager/conf.d
-#ignore interfaces other than wlan0
-cp ./ig-NetworkManager-baseline.conf /etc/NetworkManager/conf.d
+bash ./gentoo-NetworkManager.sh
 cp ./gentoo-rules-save /var/lib/iptables/rules-save
 cp ./issue /etc/issue
 #TODO ipv6 version of this
