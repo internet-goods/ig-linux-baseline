7cb1f4980890467690b46977b94d48601f3d2cde
internet-goods <4577617+internet-goods@users.noreply.github.com>
2024-03-26 07:44:02 -0500

    Update securityonioncentos.sh
    ccze

--- securityonioncentos.sh
+++ securityonioncentos.sh
@@ -1,6 +1,6 @@
 dnf -y install epel-release
 #monitoring
-dnf -y install htop sysstat iotop smartmontools lsof
+dnf -y install htop sysstat iotop smartmontools lsof ccze
 systemctl enable smartd
 #client apps
 dnf -y install screen alpine lynx
