96d9f3ed5d6f53d7d9c3764e22477e5790ebf331
internet-goods <4577617+internet-goods@users.noreply.github.com>
2024-03-27 19:52:37 -0500

    Update securityonioncentos.sh
    lmsensors

--- securityonioncentos.sh
+++ securityonioncentos.sh
@@ -1,9 +1,11 @@
 dnf -y install epel-release
 #monitoring
-dnf -y install htop sysstat iotop smartmontools lsof ccze
+dnf -y install htop sysstat iotop smartmontools lsof lm_sensors
+yes|sensors-detect
+sensors
 systemctl enable smartd
 #client apps
-dnf -y install screen alpine lynx
+dnf -y install screen alpine lynx ccze
 #hardening
 dnf -y install lynis openscap openscap-utils scap-security-guide
 lynis audit system
