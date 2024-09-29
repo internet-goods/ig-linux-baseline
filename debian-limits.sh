b02c09b5c578cd06bba1c432be90edbe391af8f2
internet-goods <4577617+internet-goods@users.noreply.github.com>
2024-09-29 09:58:11 -0500

    Update and rename debian-limits.sh to limits.sh

--- debian-limits.sh
+++
@@ -1,1 +0,0 @@
-echo "* hard core 0" > /etc/security/limits.d/kali-baseline.conf
---
+++ limits.sh
@@ -0,0 +1,2 @@
+echo "* hard core 0" > /etc/security/limits.d/ig-baseline.conf
+echo "* soft core 0" > /etc/security/limits.d/ig-baseline.conf
