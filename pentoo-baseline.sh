92d5828b7197831595c8126630c217ec00012a4f
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-01-27 17:31:18 -0600

    Update pentoo-baseline.sh

--- pentoo-baseline.sh
+++ pentoo-baseline.sh
@@ -7,3 +7,5 @@ cp ./gentoo-rules-save /var/lib/iptables/rules-save
 #TODO ipv6 version of this
 cp ./issue /etc/issue
 bash ./auditd.sh
+cp ig-pentoo-ssh.conf /etc/ssh/sshd_config.d
+/etc/init.d/sshd restart
