06ec1dbb0926ea6d15772905d8c33a54c1123421
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-02-26 20:44:12 -0600

    Delete debian-ntp.sh

--- debian-ntp.sh
+++
@@ -1,6 +0,0 @@
-apt-get -y install systemd-timesyncd
-echo "[Time]" > /etc/systemd/timesyncd.conf
-echo "NTP=node40" >> /etc/systemd/timesyncd.conf
-systemctl daemon-reload
-timedatectl set-ntp off
-timedatectl set-ntp on
