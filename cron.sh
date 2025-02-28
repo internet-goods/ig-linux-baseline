8870d2b215f486d5bb626c3b5928834d32fb816f
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-02-27 18:34:00 -0600

    Delete cron.sh

--- cron.sh
+++
@@ -1,7 +0,0 @@
-chmod 600 /etc/crontab
-chmod 700 /etc/cron.d
-chmod 700 /etc/cron.daily
-chmod 700 /etc/cron.hourly
-chmod 700 /etc/cron.weekly
-chmod 700 /etc/cron.monthly
-chmod 600 /etc/cron.deny
