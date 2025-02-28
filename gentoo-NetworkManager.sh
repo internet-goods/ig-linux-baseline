bf14684d517ddc6de9eefa6e4d26d90a43f0e89f
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-02-27 18:13:07 -0600

    Delete gentoo-NetworkManager.sh

--- gentoo-NetworkManager.sh
+++
@@ -1,3 +0,0 @@
-echo [main] > /etc/NetworkManager/NetworkManager.conf
-echo plugins=keyfile >> /etc/NetworkManager/NetworkManager.conf
-echo hostname-mode=none >> /etc/NetworkManager/NetworkManager.conf
