832e7fa5ed62d32315810ef07d64b0511793378b
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-02-27 18:26:01 -0600

    Update debian-baseline.sh

--- debian-baseline.sh
+++ debian-baseline.sh
@@ -103,3 +103,23 @@ sed -i 's/22/27/g' /etc/login.defs
 echo "SHA_CRYPT_MIN_ROUNDS 800000" >> /etc/login.defs
 echo "SHA_CRYPT_MAX_ROUNDS 900000" >> /etc/login.defs
 echo "PASS_MIN_DAYS 1" >> /etc/login.defs
+
+#disable modules
+echo "install cramfs /bin/true" > /etc/modprobe.d/debian-baseline.conf
+echo "install freevxfs /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install jffs2 /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install hfs /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install hfsplus /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install squashfs /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install udf /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install vfat /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "options ipv6 disable=1" >> /etc/modprobe.d/debian-baseline.conf
+echo "install dccp /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install sctp /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install rds /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install tipc /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+#echo "-w /sbin/modprobe -p x -k modules" >> /etc/audit/rules.d/debian-baseline.conf
+#/etc/modprobe.d/debian-baseline.confsystemctl -w kernel.yama.ptrace_scope=3
+echo "install firewire-ohci /bin/true" >>  /etc/modprobe.d/debian-baseline.conf
+echo "install firewire-sbp2 /bin/true" >>  /etc/modprobe.d/debian-baseline.conf
+echo "install usb-storage /bin/true" >> /etc/modprobe.d/debian-baseline.conf
