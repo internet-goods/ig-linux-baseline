89641cbbb40ae1670519fb8fc806a27f8d8d3221
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-02-27 18:17:55 -0600

    Update pentoo-baseline.sh

--- pentoo-baseline.sh
+++ pentoo-baseline.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
-bash ./gentoo-openrc.sh
+#bash ./gentoo-openrc.sh
+rc-service sshd start
+rc-update add sshd
+rc-service iptables start
+rc-update add iptables 
+rc-service ip6tables start
+rc-update add ip6tables
+rc-service smartd start
+rc-update add smartd
+rc-service smartd start
+rc-update add sendmail
+rc-service haveged start
+rc-update add haveged
+rc-service rngd start
+rc-update add rngd
+rc-update add thermald
+rc-service thermald start
+rc-update add lm_sensors
+rc-service lm_sensors start
+rc-update add sensord
+rc-service sensord start
+rc-update add mdraid boot
+rc-service privoxy start
+rc-update add privoxy
+rc-service auditd start
+rc-update-add auditd
+rc-update -v show
+rc-status
 bash ./gentoo-localtime.sh
 #echo [main] > /etc/NetworkManager/NetworkManager.conf
 #echo plugins=keyfile >> /etc/NetworkManager/NetworkManager.conf
