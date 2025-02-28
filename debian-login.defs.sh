f0b357657c7a1ff9601f2bf49bcf65eda1867c57
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-02-27 18:24:23 -0600

    Delete debian-login.defs.sh

--- debian-login.defs.sh
+++
@@ -1,4 +0,0 @@
-sed -i 's/22/27/g' /etc/login.defs
-echo "SHA_CRYPT_MIN_ROUNDS 800000" >> /etc/login.defs
-echo "SHA_CRYPT_MAX_ROUNDS 900000" >> /etc/login.defs
-echo "PASS_MIN_DAYS 1" >> /etc/login.defs
