f79b48deedd5e87c896e818c99cc28887319e9df
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-03-13 18:48:04 -0500

    Update securityonion-baseline.sh

--- securityonion-baseline.sh
+++ securityonion-baseline.sh
@@ -98,6 +98,20 @@ bash cron.sh
 #systemctl list-unit-files --state=enabled
 
 
+
+#aide --init
+#cp /var/lib/aide.db.new.gz /var/lib/aide.db.gz
+# update-grub 
+
+#disable networkmanagement of wifi for kismet
+#plugins=keyfile,ifcfg-rh
+sed -i "s/#plugins=keyfile,ifcfg-rh/plugins=keyfile/g" /etc/NetworkManager/NetworkManager.conf
+echo "[keyfile]" >> /etc/NetworkMAnager/NetworkManager.conf
+echo "unmanaged-devices=type:wifi" >> /etc/NetworkMAnager/NetworkManager.conf
+service NetworkManager restart
+docker run -d --rm -p 2501:2501 -it --privileged --net=host --pid=host finchsec/kismet
+
+#audit compliance
 oscap info /usr/share/xml/scap/ssg/content/ssg-ol9-ds.xml 
 mkdir $(date -I)
 cd $(date -I)
@@ -115,29 +129,9 @@ oscap xccdf eval --fetch-remote-resources \
 --profile xccdf_org.ssgproject.content_profile_standard \
 --report xccdf_org.ssgproject.content_profile_standard.html \
 /usr/share/xml/scap/ssg/content/ssg-ol9-ds.xml
+
 lynis audit system
 
-#git clone https://github.com/docker/docker-bench-security
-#cd docker-bench-security
-#sh docker-bench-security.sh
-#cd ..
-#wget https://rpmfind.net/linux/centos-stream/9-stream/AppStream/x86_64/os/Packages/golang-src-1.22.5-2.el9.noarch.rpm
-#wget https://rpmfind.net/linux/centos-stream/9-stream/AppStream/x86_64/os/Packages/golang-race-1.22.5-2.el9.x86_64.rpm
-#wget https://rpmfind.net/linux/centos-stream/9-stream/AppStream/x86_64/os/Packages/golang-bin-1.22.5-2.el9.x86_64.rpm
-#wget https://rpmfind.net/linux/centos-stream/9-stream/AppStream/x86_64/os/Packages/golang-1.22.5-2.el9.x86_64.rpm
-#rpm -Uvh golang-1.22.5-2.el9.x86_64.rpm golang-bin-1.22.5-2.el9.x86_64.rpm golang-race-1.22.5-2.el9.x86_64.rpm golang-src-1.22.5-2.el9.noarch.rpm
-
-
-
-#GRUB_CMDLINE_LINUX="pcie_aspm=off pcie_port_pm=off"
-#aide --init
-#cp /var/lib/aide.db.new.gz /var/lib/aide.db.gz
-# update-grub 
-
-#disable networkmanagement of wifi for kismet
-#plugins=keyfile,ifcfg-rh
-sed -i "s/#plugins=keyfile,ifcfg-rh/plugins=keyfile/g" /etc/NetworkManager/NetworkManager.conf
-echo "[keyfile]" >> /etc/NetworkMAnager/NetworkManager.conf
-echo "unmanaged-devices=type:wifi" >> /etc/NetworkMAnager/NetworkManager.conf
-service NetworkManager restart
-docker run -d --rm -p 2501:2501 -it --privileged --net=host --pid=host finchsec/kismet
+git clone https://github.com/docker/docker-bench-security
+cd docker-bench-security
+sh ./docker-bench-security.sh -l docker-bench-security-$(date -I).log
