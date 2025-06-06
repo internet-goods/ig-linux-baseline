1e8280e9008bcdc7b202110bb79d553c85c21c83
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-06-06 01:20:12 -0500

    Update kali-baseline.sh

--- kali-baseline.sh
+++ kali-baseline.sh
@@ -115,4 +115,18 @@ apt -y install nvidia-driver-libs:i386
 
 apt -y install htcondor
 apt -y install inxi
-apt -y install docker.io docker-doc docker-compose containerd
+#apt -y install docker.io docker-doc docker-compose containerd
+#systemctl enable docker
+#systemctl start docker
+#docker info
+#https://greenbone.github.io/docs/latest/22.4/container/index.html
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
