8de20dbe4e442d552ff0ee58a723eb5368ad5add
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-07-18 17:20:45 -0500

    Update kali-baseline.sh

--- kali-baseline.sh
+++ kali-baseline.sh
@@ -139,6 +139,14 @@ apt install steam-installer
 apt-get upgrade steam -f
 apt -y install nvidia-driver-libs:i386
 
+#https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+apt install -y nvidia-container-toolkit
+
 apt -y install htcondor
 
 #apt -y install docker.io docker-doc docker-compose containerd
