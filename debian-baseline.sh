d03380a836d0bfd7019ce63b6bd83f3cd20c74a6
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-01-20 18:26:03 -0600

    Refactor package installations in debian-baseline.sh
    Removed several package installations and added conditional installation for optional packages. Updated the installation of signal-desktop and reorganized package installation commands.

--- debian-baseline.sh
+++ debian-baseline.sh
@@ -49,9 +49,104 @@ update-rc.d ssh enable
 service ssh start
 
 apt -y install apt-show-versions
-apt -y install multimon-ng
 apt -y install screen
-apt -y install libnl*
+apt-get -y install  alpine
+apt-get -y install htop iotop iftop lm-sensors audispd-plugins usbguard sysstat hdparm
+apt-get -y install bc
+#pam
+apt-get -y install libpam-tmpdir libpam-usb 
+apt-get -y install libpam-cracklib
+apt-get -y install libpam-passwdqc
+#apt
+apt-get -y install apt-listbugs debian-goodies needrestart debsecan apt-transport-https
+apt-get -y install debsums
+apt-get -y install unattended-upgrades apt-listchanges
+apt-get -y install iptables-persistent
+#dedup
+apt-get -y install rdfind
+apt-get -y install python3-pip
+apt-get -y install python3-csvkit
+apt-get -y install jq
+apt-get -y install lynis
+
+sed -i 's/22/27/g' /etc/login.defs
+echo "SHA_CRYPT_MIN_ROUNDS 800000" >> /etc/login.defs
+echo "SHA_CRYPT_MAX_ROUNDS 900000" >> /etc/login.defs
+echo "PASS_MIN_DAYS 1" >> /etc/login.defs
+
+#disable modules
+echo "install cramfs /bin/true" > /etc/modprobe.d/debian-baseline.conf
+echo "install freevxfs /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install jffs2 /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install hfs /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install hfsplus /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install squashfs /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install udf /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install vfat /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "options ipv6 disable=1" >> /etc/modprobe.d/debian-baseline.conf
+echo "install dccp /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install sctp /bin/true" >> /etc/modprobe.d/debian-bapt -y install libnl*aseline.conf
+echo "install rds /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+echo "install tipc /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+#echo "-w /sbin/modprobe -p x -k modules" >> /etc/audit/rules.d/debian-baseline.conf
+#/etc/modprobe.d/debian-baseline.confsystemctl -w kernel.yama.ptrace_scope=3
+echo "install firewire-ohci /bin/true" >>  /etc/modprobe.d/debian-baseline.conf
+echo "install firewire-sbp2 /bin/true" >>  /etc/modprobe.d/debian-baseline.conf
+echo "install usb-storage /bin/true" >> /etc/modprobe.d/debian-baseline.conf
+
+echo umask 022 > /etc/profile.d/debian-baseline.sh
+
+chmod 600 /etc/crontab
+chmod 700 /etc/cron.d
+chmod 700 /etc/cron.daily
+chmod 700 /etc/cron.hourly
+chmod 700 /etc/cron.weekly
+chmod 700 /etc/cron.monthly
+chmod 600 /etc/cron.deny
+# Second part: conditional
+if [ "$1" == "-full" ]; then
+    echo "true"
+#optionals
+#benchmarking
+apt-get -y install boinc-client boinc-manager iozone3 sysbench
+#browsers
+apt-get -y upgrade chromium firefox w3m lynx
+apt-get -y install tiger
+apt-get -y install libreoffice-calc
+apt -y install sdcc
+apt -y install qemu-kvm qemu-system-arm qemu-system-x86 virt-manager
+apt -y install speedtest-cli
+apt -y install mdadm
+
+
+apt -y install x11-apps inxi
+#media
+apt-get -y install mpv smplayer vlc geeqie gimp sox
+#hamradio
+apt-get -y install gnuradio rtl-sdr hackrf gqrx-sdr kismet* 
+
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+# 2. Add our repository to your list of repositories
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+# 3. Update your package database and install signal
+sudo apt update && sudo apt install signal-desktop
+#proxy
+apt-get -y install privoxy docker-compose
+#vuln scanners
+
+apt-get -y install sqlmap nikto wapiti 
+ apt-get -y install suricata* psad chaosreader ipcalc driftnet arpwatch arpon
+#selinux
+#apt-get -y install setools setools-gui selinux-policy-default selinux-basics 
+#av
+#apt-get -y install clamav yara rkhunter aide chkrootkit acct logwatch tripwire fail2ban
+#forensics
+apt-get -y install strace gdb dcfldd pngcheck autopsy ddrescue 
+
 apt -y install libpcre3-dev
 apt -y install libnm-dev
 apt -y install libnl-3-dev
@@ -71,7 +166,7 @@ apt -y install ccze
 apt -y install horst
 apt -y install hashcat*
 apt -y install nvidia-cuda-toolkit
-apt-get -y install sendmail alpine
+apt -y install libnl*
 apt-get -y install bettercap
 apt-get -y install rng-tools
 apt-get -y install mmc-utils
@@ -79,91 +174,5 @@ apt-get -y install ethtool
 apt-get -y install ntpstat ntpdate ntp
 apt-get -y install gkrellm* lshw
 apt-get -y install libimage-exiftool-perl zsteg
-  apt-get -y install htop iotop iftop lm-sensors audispd-plugins usbguard sysstat hdparm
-  apt-get -y install suricata* psad chaosreader ipcalc driftnet arpwatch arpon
-#selinux
-#apt-get -y install setools setools-gui selinux-policy-default selinux-basics 
-#av
-#apt-get -y install clamav yara rkhunter aide chkrootkit acct logwatch tripwire fail2ban
-#forensics
-apt-get -y install strace gdb dcfldd pngcheck autopsy ddrescue 
-#media
-apt-get -y install mpv smplayer vlc geeqie gimp sox
-#hamradio
-apt-get -y install gnuradio rtl-sdr hackrf gqrx-sdr kismet* 
-#benchmarking
-apt-get -y install boinc-client boinc-manager iozone3 sysbench
-#vuln scanners
-
-apt-get -y install sqlmap nikto wapiti 
-#browsers
-apt-get -y upgrade chromium firefox w3m lynx
-#proxy
-apt-get -y install privoxy docker-compose
-#math
-apt-get -y install libreoffice-calc
-apt-get -y install bc
-#pam
-apt-get -y install libpam-tmpdir libpam-usb libpam-cracklib libpam-passwdqc
-#apt
-apt-get -y install apt-listbugs debian-goodies needrestart debsecan apt-transport-https
-apt-get -y install debsums
-apt-get -y install unattended-upgrades apt-listchanges
-#hardening
-apt-get -y install tiger
-apt-get -y install iptables-persistent
-#dedup
-apt-get -y install rdfind
-apt-get -y install python3-pip
-apt-get -y install python3-csvkit
-apt-get -y install jq
-apt-get -y install lynis
-apt -y install sdcc
-apt -y install qemu-kvm qemu-system-arm qemu-system-x86 virt-manager
-apt -y install speedtest-cli
-apt -y install mdadm
-apt -y install x11-apps inxi
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
-
-# 2. Add our repository to your list of repositories
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
-  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-
-# 3. Update your package database and install signal
-sudo apt update && sudo apt install signal-desktop
-
-sed -i 's/22/27/g' /etc/login.defs
-echo "SHA_CRYPT_MIN_ROUNDS 800000" >> /etc/login.defs
-echo "SHA_CRYPT_MAX_ROUNDS 900000" >> /etc/login.defs
-echo "PASS_MIN_DAYS 1" >> /etc/login.defs
-
-#disable modules
-echo "install cramfs /bin/true" > /etc/modprobe.d/debian-baseline.conf
-echo "install freevxfs /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-echo "install jffs2 /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-echo "install hfs /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-echo "install hfsplus /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-echo "install squashfs /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-echo "install udf /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-echo "install vfat /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-echo "options ipv6 disable=1" >> /etc/modprobe.d/debian-baseline.conf
-echo "install dccp /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-echo "install sctp /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-echo "install rds /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-echo "install tipc /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-#echo "-w /sbin/modprobe -p x -k modules" >> /etc/audit/rules.d/debian-baseline.conf
-#/etc/modprobe.d/debian-baseline.confsystemctl -w kernel.yama.ptrace_scope=3
-echo "install firewire-ohci /bin/true" >>  /etc/modprobe.d/debian-baseline.conf
-echo "install firewire-sbp2 /bin/true" >>  /etc/modprobe.d/debian-baseline.conf
-echo "install usb-storage /bin/true" >> /etc/modprobe.d/debian-baseline.conf
-
-echo umask 022 > /etc/profile.d/debian-baseline.sh
-
-chmod 600 /etc/crontab
-chmod 700 /etc/cron.d
-chmod 700 /etc/cron.daily
-chmod 700 /etc/cron.hourly
-chmod 700 /etc/cron.weekly
-chmod 700 /etc/cron.monthly
-chmod 600 /etc/cron.deny
+apt -y install multimon-ng
+fi
