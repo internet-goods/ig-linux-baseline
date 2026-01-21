ed073fb93073b3ce4292175a5db766bb03bc65fe
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-01-20 21:02:24 -0600

    Add sysctl settings for network performance tuning

--- debian-sysctl.conf.sh
+++ debian-sysctl.conf.sh
@@ -24,3 +24,23 @@ echo "kernel.yama.ptrace_scope=3"   >> /etc/sysctl.conf
 echo "kernel.perf_event_max_sample_rate = 100000"  >> /etc/sysctl.conf
 # Disable the "auto-throttle" mechanism (optional)
 echo "kernel.perf_cpu_time_max_percent = 25" >> /etc/sysctl.conf
+# Increase the max number of open connections
+echo "net.core.somaxconn = 65535"  >> /etc/sysctl.conf
+# Increase the number of SYN requests allowed
+echo "net.ipv4.tcp_max_syn_backlog = 65535" >> /etc/sysctl.conf
+# Increase the maximum amount of option memory for networking
+echo "net.core.rmem_max = 16777216" >> /etc/sysctl.conf
+echo "net.core.wmem_max = 16777216" >> /etc/sysctl.conf
+# Reuse TIME_WAIT sockets
+echo "net.ipv4.tcp_tw_reuse = 1" >> /etc/sysctl.conf
+# Expand local port range for high-volume delivery
+echo "net.ipv4.ip_local_port_range = 1024 65535" >> /etc/sysctl.conf
+# Speed up the closing of idle connections
+echo "net.ipv4.tcp_fin_timeout = 15" >> /etc/sysctl.conf
+# Enable SYN cookie protection
+echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf
+# Disable packet forwarding (unless this is a gateway)
+echo "net.ipv4.ip_forward = 0" >> /etc/sysctl.conf
+# Prevent IP Spoofing
+echo "net.ipv4.conf.all.rp_filter = 1" >> /etc/sysctl.conf
+echo "net.ipv4.conf.default.rp_filter = 1" >> /etc/sysctl.conf
