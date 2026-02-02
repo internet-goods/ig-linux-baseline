e3e4b7c9efe7091fbcf4aa3f4cc675c4f9a10207
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-02-01 23:08:38 -0600

    Update ig-docker-omgwtfssl.sh

--- ig-docker-omgwtfssl.sh
+++ ig-docker-omgwtfssl.sh
@@ -1,1 +1,2 @@
-docker run -v certs:/certs  -e SSL_SIZE=8192 -e SSL_EXPIRE=365 -e SSL_SUBJECT=$1 paulczar/omgwtfssl
+docker run -v /tmp/certs:/certs  -e SSL_SIZE=8192 -e SSL_EXPIRE=365 -e SSL_SUBJECT=$1 paulczar/omgwtfssl
+mv /tmp/certs /root/$1.certs
