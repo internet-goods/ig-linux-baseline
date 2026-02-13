eac9f432702152f6526a99cc3338d7a531cd3110
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-02-12 21:30:07 -0600

    Update so-index-delete-red.sh

--- so-index-delete-red.sh
+++ so-index-delete-red.sh
@@ -1,1 +1,1 @@
-so-index-list |grep -v green|awk '{print "so-elasticsearch-query " $3 " -XDELETE"}' > so-index-delete-red-$(date-I).sh
+so-index-list |grep -v green|awk '{print "so-elasticsearch-query " $3 " -XDELETE"}' > so-index-delete-red-$(date -I).sh
