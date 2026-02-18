e4b11c929bda5bba0640b4dbb3f1d8aaba68a152
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-02-17 18:48:28 -0600

    Update package installation in securityonion-baseline.sh
    Removed 'inxi' from the installation list and updated comments regarding 'lm_sensors'.

--- securityonion-baseline.sh
+++ securityonion-baseline.sh
@@ -3,7 +3,8 @@ echo "iptables -I INPUT -i bond0 -d 255.255.255.255 -s 0.0.0.0 -p udp --dport 67
 dnf -y install epel-release 
 dnf -y install lsb-release
 #monitoring
-dnf -y install htop sysstat iotop smartmontools lsof hddtemp mcelog psacct usbutils iftop inxi
+dnf -y install htop sysstat iotop smartmontools lsof hddtemp mcelog psacct usbutils iftop
+#inxi needs lm_sensors
 #lm_sensors has been removed
 #yes|sensors-detect
 #dnf -y install bluez rtl-sdr hcxtools
