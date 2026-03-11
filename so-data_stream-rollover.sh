9c0dbb71afc682efa701259c3798e9fe422ef6d7
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-03-10 20:25:10 -0500

    Fix command order in rollover script

--- so-data_stream-rollover.sh
+++ so-data_stream-rollover.sh
@@ -1,1 +1,1 @@
-so-elasticsearch-query _data_stream|jq |grep name | grep -v index_ | grep -v @timestamp |awk -F\" '{print "so-elasticsearch-query -X POST "$4"/_rollover"}' > so-data_stream-rollover-$(date -I).sh
+so-elasticsearch-query _data_stream|jq |grep name | grep -v index_ | grep -v @timestamp |awk -F\" '{print "so-elasticsearch-query "$4"/_rollover -X POST"}' > so-data_stream-rollover-$(date -I).sh
