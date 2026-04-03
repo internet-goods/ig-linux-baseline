44bf029c735b65c2d8967f64771e777b68964874
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-04-03 06:40:07 -0500

    Update docker-openvas.sh

--- docker-openvas.sh
+++ docker-openvas.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export DOWNLOAD_DIR=$HOME/greenbone-community-container && mkdir -p $DOWNLOAD_DIR
-cd $DOWNLOAD_DIR && curl -f -L https://greenbone.github.io/docs/latest/_static/docker-compose-22.4.yml -o docker-compose.yml
+cd $DOWNLOAD_DIR
+curl -f -O -L https://greenbone.github.io/docs/latest/_static/compose.yaml -o docker-compose.yml
 docker compose -f $DOWNLOAD_DIR/docker-compose.yml -p greenbone-community-edition pull
 docker compose -f $DOWNLOAD_DIR/docker-compose.yml -p greenbone-community-edition up -d
