8074a8cbe34f534ddd94e66f957e5302d5990622
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-06-11 21:42:33 -0500

    Update ig-docker-omgwtfssl.sh

--- ig-docker-omgwtfssl.sh
+++ ig-docker-omgwtfssl.sh
@@ -20,7 +20,7 @@ ORG=$(echo $LOCATION_DATA | jq -r '.org')
 # Create the openssl.cnf file
 cat <<EOF > $1.openssl.cnf
 [ req ]
-default_bits       = 8192
+default_bits       = 4096
 distinguished_name = req_distinguished_name
 req_extensions     = v3_req
 prompt             = no
@@ -45,5 +45,5 @@ DNS.3 = mail.$DOMAIN
 EOF
 
 echo "Success! openssl.cnf created for $DOMAIN in $CITY, $STATE."
-docker run -v /tmp/certs:/certs -e SSL_CONFIG=$1.openssl.cnf -e SSL_SIZE=8192 -e SSL_EXPIRE=365 -e CA_SUBJECT=$1-ca -e CA_KEY=$1-ca-key.pem -e CA_CERT=$1-ca.pem -e SSL_KEY=$1-cert-key.pem -e SSL_CSR=$1-cert.csr -e SSL_CERT=$1-cert.pem -e SSL_SUBJECT=$1 -e SSL_DNS=www.$1,mail.$1 paulczar/omgwtfssl
+docker run -v /tmp/certs:/certs -e SSL_CONFIG=$1.openssl.cnf -e SSL_SIZE=4096 -e SSL_EXPIRE=3650 -e CA_SUBJECT=$1-ca -e CA_KEY=$1-ca-key.pem -e CA_CERT=$1-ca.pem -e SSL_KEY=$1-cert-key.pem -e SSL_CSR=$1-cert.csr -e SSL_CERT=$1-cert.pem -e SSL_SUBJECT=$1 -e SSL_DNS=www.$1,mail.$1 paulczar/omgwtfssl
 mv /tmp/certs $1.certs
