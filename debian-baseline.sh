36fcc164b3a94e3c06a805bb8d3345d262e98e1e
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-06-13 13:06:34 -0500

    Update debian-baseline.sh

--- debian-baseline.sh
+++ debian-baseline.sh
@@ -4,24 +4,21 @@ sed -i 's/no/yes/g' /etc/audit/plugins.d/syslog.conf
 debian-iptables.sh
 echo "* hard core 0" > /etc/security/limits.d/ig-baseline.conf
 echo "* soft core 0" > /etc/security/limits.d/ig-baseline.conf
-
-debian-modprobe.sh
+#debian-modprobe.sh
 mount -o remount,hidepid=2 /proc
 debian-pam-common-password.sh
-debian-profile.shinxi
+#debian-profile.sh
 #debian-rsyslog.conf.sh
 debian-sysctl.conf.sh
 apt -y install sysstat
 systemctl enable sysstat
 sed -i 's/false/true/g' /etc/default/sysstat
-
 update-rc.d acct enable
 update-rc.d ssh enable
 service ssh start
 service acct start
 cp issue /etc/issue
 cp issue /etc/issue.net
-
 # Open inbound ssh(tcp port 22) connections
 #5.2 ssh cfg
 chmod og-rwx /etc/ssh/sshd_config
@@ -48,10 +45,9 @@ echo "Port 220" >> /etc/ssh/sshd_config.d/baseline.conf
 service sshd reload
 update-rc.d ssh enable
 service ssh start
-
 apt -y install apt-show-versions
 apt -y install screen
-apt-get -y install  alpine
+apt-get -y install alpine
 apt-get -y install htop iotop iftop lm-sensors audispd-plugins usbguard sysstat hdparm
 apt-get -y install bc
 apt-get -y install net-tools
@@ -98,7 +94,6 @@ echo "install usb-storage /bin/true" >> /etc/modprobe.d/debian-baseline.conf
 
 echo umask 022 > /etc/profile.d/debian-baseline.sh
 
-
 chmod 600 /etc/crontab
 chmod 700 /etc/cron.d
 chmod 700 /etc/cron.daily
@@ -106,50 +101,45 @@ chmod 700 /etc/cron.hourly
 chmod 700 /etc/cron.weekly
 chmod 700 /etc/cron.monthly
 chmod 600 /etc/cron.deny
-# Second part: conditional
-if [ "$1" == "-full" ]; then
-    echo "true"
+
 #optionals
 #benchmarking
 apt-get -y install boinc-client boinc-manager iozone3 sysbench
 #browsers
-apt-get -y upgrade chromium firefox w3m lynx
+apt -y install w3m lynx
 apt-get -y install tiger
 apt-get -y install libreoffice-calc
 apt -y install sdcc
 apt -y install qemu-kvm qemu-system-arm qemu-system-x86 virt-manager
 apt -y install speedtest-cli
 apt -y install mdadm
-
-
 apt -y install x11-apps inxi
 #media
 apt-get -y install mpv smplayer vlc geeqie gimp sox
 #hamradio
-apt-get -y install gnuradio rtl-sdr hackrf gqrx-sdr kismet* 
+apt-get -y install gnuradio rtl-sdr hackrf gqrx-sdr 
+apt -y install kismet* 
 
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+#wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+#cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
 
 # 2. Add our repository to your list of repositories
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
-  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+#echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+#  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 
 # 3. Update your package database and install signal
-sudo apt update && sudo apt install signal-desktop
+#sudo apt update && sudo apt install signal-desktop
 #proxy
 apt-get -y install privoxy docker-compose
 #vuln scanners
-
 apt-get -y install sqlmap nikto wapiti 
- apt-get -y install suricata* psad chaosreader ipcalc driftnet arpwatch arpon
+apt-get -y install suricata* psad chaosreader ipcalc driftnet arpwatch arpon
 #selinux
 #apt-get -y install setools setools-gui selinux-policy-default selinux-basics 
 #av
-#apt-get -y install clamav yara rkhunter aide chkrootkit acct logwatch tripwire fail2ban
+apt-get -y install clamav yara rkhunter aide chkrootkit acct logwatch tripwire fail2ban
 #forensics
 apt-get -y install strace gdb dcfldd pngcheck autopsy ddrescue 
-
 apt -y install libpcre3-dev
 apt -y install libnm-dev
 apt -y install libnl-3-dev
@@ -178,4 +168,4 @@ apt-get -y install ntpstat ntpdate ntp
 apt-get -y install gkrellm* lshw
 apt-get -y install libimage-exiftool-perl zsteg
 apt -y install multimon-ng
-fi
+
