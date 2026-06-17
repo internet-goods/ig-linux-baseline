37d012960dc99fa7b87b53caab8aa312ffa42eb8
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-06-16 20:09:41 -0500

    Create modprobe-baseline.sh

---
+++ modprobe-baseline.sh
@@ -0,0 +1,16 @@
+echo "install cramfs /bin/true" > /etc/modprobe.d/ig-baseline.conf
+echo "install freevxfs /bin/true" >> /etc/modprobe.d/ig-baseline.conf
+echo "install jffs2 /bin/true" >> /etc/modprobe.d/ig-baseline.conf
+echo "install hfs /bin/true" >> /etc/modprobe.d/ig-baseline.conf
+echo "install hfsplus /bin/true" >> /etc/modprobe.d/ig-baseline.conf
+echo "install squashfs /bin/true" >> /etc/modprobe.d/ig-baseline.conf
+echo "install udf /bin/true" >> /etc/modprobe.d/ig-baseline.conf
+echo "install vfat /bin/true" >> /etc/modprobe.d/ig-baseline.conf
+echo "options ipv6 disable=1" >> /etc/modprobe.d/ig-baseline.conf
+echo "install dccp /bin/true" >> /etc/modprobe.d/ig-baseline.conf
+echo "install sctp /bin/true" >> /etc/modprobe.d/ig-baseline.conf
+echo "install rds /bin/true" >> /etc/modprobe.d/ig-baseline.conf
+echo "install tipc /bin/true" >> /etc/modprobe.d/ig-baseline.conf
+echo "install firewire-ohci /bin/true" >>  /etc/modprobe.d/ig-baseline.conf
+echo "install firewire-sbp2 /bin/true" >>  /etc/modprobe.d/ig-baseline.conf
+echo "install usb-storage /bin/true" >> /etc/modprobe.d/ig-baseline.conf
