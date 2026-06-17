76022ab1b41d2c0d7772355ab570fb585749a14b
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-06-16 19:46:18 -0500

    Create apt-baseline.sh

---
+++ apt-baseline.sh
@@ -0,0 +1,140 @@
+apt-get -y install alpine
+apt-get -y install apt-listbugs debian-goodies needrestart debsecan apt-transport-https
+apt-get -y install bc
+apt-get -y install boinc-client boinc-manager iozone3 sysbench
+apt-get -y install clamav yara rkhunter aide chkrootkit acct logwatch  fail2ban
+apt-get -y install debsums
+apt-get -y install ethtool
+apt-get -y install gnuradio rtl-sdr hackrf gqrx-sdr 
+apt-get -y install htop iotop iftop lm-sensors audispd-plugins  sysstat hdparm
+apt-get -y install ipcalc
+apt-get -y install iptables-persistent
+apt-get -y install jq
+apt-get -y install libimage-exiftool-perl zsteg
+apt-get -y install libpam-cracklib
+apt-get -y install libpam-passwdqc
+apt-get -y install libpam-tmpdir libpam-usb 
+apt-get -y install lshw inxi
+apt-get -y install lynis
+apt-get -y install mmc-utils
+apt-get -y install mpv smplayer vlc geeqie gimp sox
+apt-get -y install net-tools
+apt-get -y install ntpstat ntpdate ntp
+apt-get -y install privoxy docker-compose
+apt-get -y install python3-csvkit
+apt-get -y install python3-pip
+apt-get -y install rdfind
+apt-get -y install rng-tools
+#apt-get -y install setools setools-gui selinux-policy-default selinux-basics 
+apt-get -y install sqlmap nikto wapiti 
+apt-get -y install strace gdb   
+apt-get -y install tiger
+apt-get -y install unattended-upgrades apt-listchanges
+apt -y install apt-show-versions
+apt -y install audit
+apt -y install ccze
+apt -y install gdebi
+apt -y install hashcat hashcat-nvidia
+apt -y install horst
+apt -y install libnl*
+apt -y install libnl-3-dev
+apt -y install libnm-dev
+apt -y install libpcre3-dev
+apt -y install libprotobuf-c-dev
+apt -y install libprotobuf-dev
+apt -y install libsensors-dev
+apt -y install libsqlite3-dev
+apt -y install libssl-dev
+apt -y install libusb-1.0-0-dev
+apt -y install libwebsockets-dev
+apt -y install mdadm
+apt -y install multimon-ng
+#apt -y install nvidia-cuda-toolkit enable nonfree repo
+apt -y install pdfgrep
+apt -y install protobuf-compiler
+apt -y install python3-full python3-venv
+apt -y install qemu-kvm qemu-system-arm qemu-system-x86 virt-manager
+apt -y install screen
+apt -y install sdcc
+apt -y install smartmontools
+apt -y install sysstat
+apt -y install v4l-utils
+apt -y install w3m lynx chromium
+apt -y install wavemon
+apt -y install x11-apps 
+apt -y linux-headers-$(uname -r)
+#selinux
+#suricata* psad chaosreader ipcalc driftnet arpwatch arpon
+#tripwire
+[Up: 32m] [Load: 0.20] [Temp: 36.0°C] [IO-In: 0MB] [Net-In: 121MB]
+n00m@node47:~$ vim b
+[Up: 32m] [Load: 0.21] [Temp: 36.0°C] [IO-In: 0MB] [Net-In: 121MB]
+n00m@node47:~$ cat b
+apt -y install alpine
+apt -y install apt-listbugs debian-goodies needrestart debsecan apt-transport-https
+apt -y install bc
+apt -y install boinc-client boinc-manager iozone3 sysbench
+apt -y install clamav yara rkhunter aide chkrootkit acct logwatch  fail2ban
+apt -y install debsums
+apt -y install ethtool
+apt -y install gnuradio rtl-sdr hackrf gqrx-sdr 
+apt -y install htop iotop iftop lm-sensors audispd-plugins  sysstat hdparm
+apt -y install ipcalc
+apt -y install iptables-persistent
+apt -y install jq
+apt -y install libimage-exiftool-perl zsteg
+apt -y install libpam-cracklib
+apt -y install libpam-passwdqc
+apt -y install libpam-tmpdir libpam-usb 
+apt -y install lshw inxi
+apt -y install lynis
+apt -y install mmc-utils
+apt -y install mpv smplayer vlc geeqie gimp sox
+apt -y install net-tools
+apt -y install ntpstat ntpdate ntp
+apt -y install privoxy docker-compose
+apt -y install python3-csvkit
+apt -y install python3-pip
+apt -y install rdfind
+apt -y install rng-tools
+#apt -y install setools setools-gui selinux-policy-default selinux-basics 
+apt -y install sqlmap nikto wapiti 
+apt -y install strace gdb   
+apt -y install tiger
+apt -y install unattended-upgrades apt-listchanges
+apt -y install apt-show-versions
+apt -y install audit
+apt -y install ccze
+apt -y install gdebi
+apt -y install hashcat hashcat-nvidia
+apt -y install horst
+apt -y install libnl*
+apt -y install libnl-3-dev
+apt -y install libnm-dev
+apt -y install libpcre3-dev
+apt -y install libprotobuf-c-dev
+apt -y install libprotobuf-dev
+apt -y install libsensors-dev
+apt -y install libsqlite3-dev
+apt -y install libssl-dev
+apt -y install libusb-1.0-0-dev
+apt -y install libwebsockets-dev
+apt -y install mdadm
+apt -y install multimon-ng
+#apt -y install nvidia-cuda-toolkit enable nonfree repo
+apt -y install pdfgrep
+apt -y install protobuf-compiler
+apt -y install python3-full python3-venv
+apt -y install qemu-kvm qemu-system-arm qemu-system-x86 virt-manager
+apt -y install screen
+apt -y install sdcc
+apt -y install smartmontools
+apt -y install sysstat
+apt -y install v4l-utils
+apt -y install w3m lynx chromium
+apt -y install wavemon
+apt -y install x11-apps 
+apt -y linux-headers-$(uname -r)
+#selinux
+#suricata* psad chaosreader ipcalc driftnet arpwatch arpon
+#tripwire
