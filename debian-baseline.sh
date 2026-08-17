37526f4192faa19b9ef2d57ee2fb17dbfbcebb89
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-08-16 22:04:08 -0500

    Update debian-baseline.sh

--- debian-baseline.sh
+++ debian-baseline.sh
@@ -30,20 +30,8 @@ echo "PASS_MIN_DAYS 1" >> /etc/login.defs
 echo umask 022 > /etc/profile.d/debian-baseline.sh
 
 ig-vulnscan.sh
-#wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-#cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
-
-# 2. Add our repository to your list of repositories
-#echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
-#  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-
-# 3. Update your package database and install signal
-#sudo apt update && sudo apt install signal-desktop
-
-
-#suricata* psad chaosreader ipcalc driftnet arpwatch arpon
-#selinux
-#apt-get -y install setools setools-gui selinux-policy-default selinux-basics 
-
-
+journalctl --vacuum-size=100M
+echo SystemMaxUse=50M >> /etc/systemd/journald.conf
+echo Storage=volatile >> /etc/systemd/journald.conf
+sed -i 's/^#NTP=.*/NTP=8.8.8.8/' /etc/systemd/timesyncd.conf
 
